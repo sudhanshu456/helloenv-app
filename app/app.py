b7d929471b526505a6e438d7af56b0cc3ac55aa2
shashankpai <paishashankk@gmail.com>
2023-10-04 12:40:27 +0000

    added the code and infra code for helloenv app

---
+++ app/app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    ENV = os.environ.get("ENV")  # Read the environment variable
+    VERSION = os.environ.get("VERSION")  #Read the version variable
+
+    if ENV == "prod":
+        return f"This is production environment (Version: {VERSION})"
+    elif ENV == "stage":
+        return f"This is staging environment with (Version: {VERSION})"
+    else:
+        return f"This is an unknown environment with (Version: {VERSION})"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
+
